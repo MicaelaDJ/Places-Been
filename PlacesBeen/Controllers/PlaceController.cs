1defdd1fffd9199d4ece1aa58a7ed62a15050eff
Micaela Jawor and Sam Stoia <sam@>
2019-02-20 09:36:04 -0800

    create file sctructure, most files

---
+++ PlacesBeen/Controllers/PlaceController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using PlacesBeen.Models;
+using System.Collections.Generic;
+
+namespace PlacesBeen.Controller
+{
+  public class PlaceController : Controller
+  {
+    
+  }
+}
---
+++ PlacesBeen/Models/Place.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace PlacesBeen.Models
+{
+  public class Place
+  {
+    private string _cityName;
+
+
+    public Place (string cityName)
+    {
+      _cityName = cityName;
+    }
+  }
+}
